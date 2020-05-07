672ee4f0a8658b8ba247021a68979d05d427cc0a
Mark <marktmmrmn@gmail.com>
2020-05-07 13:07:12 +0200

    Added upstreamchecker

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/iterum-provenance/combiner
 go 1.14
 
 require (
-	github.com/iterum-provenance/iterum-go v0.0.3
-	github.com/iterum-provenance/sidecar v0.1.8
+	github.com/iterum-provenance/cli v0.1.1
+	github.com/iterum-provenance/iterum-go v0.0.8
+	github.com/iterum-provenance/sidecar v0.1.12
 	github.com/prometheus/common v0.9.1
 )
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"sync"
 
+	"github.com/iterum-provenance/combiner/daemon"
 	"github.com/iterum-provenance/iterum-go/env"
 	"github.com/iterum-provenance/iterum-go/minio"
 	"github.com/iterum-provenance/iterum-go/transmit"
 	"github.com/iterum-provenance/iterum-go/util"
-
+	"github.com/iterum-provenance/sidecar/manager"
 	"github.com/iterum-provenance/sidecar/messageq"
 	"github.com/iterum-provenance/sidecar/store"
 
@@ -24,6 +25,7 @@ func main() {
 	downloaderSocketBridge := make(chan transmit.Serializable, downloaderSocketBridgeBufferSize)
 
 	// Download manager setup
+	daemonConfig := daemon.NewDaemonConfigFromEnv()
 	minioConf, err := minio.NewMinioConfigFromEnv() // defaults to an output setup
 	util.PanicIfErr(err, "")
 	minioConf.TargetBucket = "INVALID" // adjust such that the target output is unusable
@@ -37,9 +39,13 @@ func main() {
 	util.Ensure(err, "MessageQueue listener succesfully created and listening")
 	mqListener.Start(&wg)
 
-	uri := env.DaemonURL + "/" + env.DaemonDataset + "/pipeline_result/" + env.PipelineHash
+	uri := daemonConfig.DaemonURL + "/" + daemonConfig.Dataset + "/pipeline_result/" + env.PipelineHash
 	uploaderListener := uploader.NewListener(downloaderSocketBridge, uri)
 	uploaderListener.Start(&wg)
+
+	usChecker := manager.NewUpstreamChecker(env.ManagerURL, env.PipelineHash, env.ProcessName, 5)
+	usChecker.Start(&wg)
+	usChecker.Register <- mqListener.CanExit
 
 	wg.Wait()
 }
